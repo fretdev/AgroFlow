6e6a8293820f3cb248dd168099177d882e434094
Fretdev <ahmedibrahimprosper22@gmail.com>
2026-07-10 16:01:36 +0100

    feat: CropListingRepository setup for search and filtering

--- agroflow-backend/src/main/java/com/agroflow/backend/crop/CropListingRepository.java
+++ agroflow-backend/src/main/java/com/agroflow/backend/crop/CropListingRepository.java
@@ -3,13 +3,14 @@ package com.agroflow.backend.crop;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 
 import java.util.List;
 
 @Repository
-public interface CropListingRepository extends JpaRepository<CropListing,Long> {
+public interface CropListingRepository extends JpaRepository<CropListing,Long>, JpaSpecificationExecutor<CropListing> {
 
     List<CropListing> findAllByFarmerId(Long farmerId);
 
